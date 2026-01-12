61f774e4cecb065950fe39534d1f203a884a629c
hugofsu79 <131622641+hugofsu79@users.noreply.github.com>
2026-01-12 18:11:18 +0100

    création du tableau Job pour l'annexe

---
+++ src/data/job_annexe.ts
@@ -0,0 +1,56 @@
+export const job: Jobs[] = [{
+
+    slug: "Mes autres métiers",
+
+    details: [
+        {
+            job: "Agent de quai",
+            year: 2024,
+            entreprise: "Elis",
+            city: "Niort"
+        },
+        {
+            job: "Logisticien",
+            year: 2022,
+            entreprise: "SDIS",
+            city: "Chauray"
+        },
+        {
+            job: "Mise sous pli",
+            year: 2018,
+            entreprise: "La poste",
+            city: "Labège"
+
+        },
+        {
+            job: "Sound Designer",
+            year: 2019,
+            entreprise: "Studio H",
+            city: "Niort"
+        },
+        {
+            job: "Gérant de boutique",
+            year: 2019,
+            entreprise: "Voltex",
+            city: "Toulouse"
+        },
+        {
+            job: "Vendeur",
+            entreprise: "Duty-free",
+            year: 2018,
+            city: "Toulouse"
+        },
+        {
+            job: "Manutentionnaire",
+            entreprise: "Deya",
+            year: 2017,
+            city: "La Crèche"
+        },
+        {
+            job: "Maçon",
+            entreprise: "Moreau Laurent",
+            year: 2013,
+            city: "Vallans"
+        }
+    ],
+}]
